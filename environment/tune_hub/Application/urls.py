553f47268b18367d83558539cfbe00047bb966ed
Thanseeh yaseen <yaseenkdr27@gmail.com>
2023-03-16 21:12:05 +0530

    Home page created

--- environment/tune_hub/Application/urls.py
+++ environment/tune_hub/Application/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.login),
-    path('signup/', views.signup),
+    path('', views.login_user, name='login_user'),
+    path('signup/', views.signup, name='signup'),
+    path('index/', views.index, name='index'),
 ]
--- environment/tune_hub/Application/views.py
+++ environment/tune_hub/Application/views.py
@@ -3,8 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def login(request):
+def login_user(request):
     return render(request, 'login.html')
 
 def signup(request):
     return render(request, 'signup.html')
+
+def index(request):
+    return render(request, 'index.html')

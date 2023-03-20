7047b6e408fa56bc6a67b3b3b98a4fe9492a2411
Thanseeh yaseen <yaseenkdr27@gmail.com>
2023-03-20 21:54:20 +0530

    Update

--- environment/tune_hub/Application/views.py
+++ environment/tune_hub/Application/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.contrib.auth.models import User,auth
 from .models import Musics
 import re
+from django.db.models import Q
 
 # Create your views here.
 
@@ -95,6 +96,12 @@ def admin(request):
     total_user = data.count()
     total_music = content.count()
 
+    query = request.GET.get('q')
+    if query:
+        data = data.filter(
+            Q(first_name__icontains=query)
+        )
+
 
     if request.method == 'POST':
         first_name = request.POST['first_name']
@@ -148,7 +155,7 @@ def admin(request):
             return redirect(admin)
     
     context = {'data': data, 'total_user': total_user, 'content': content, 'total_music': total_music }
-    if 'username' in request.session:
+    if 'thanseeeeeh' in request.session:
         return render(request, 'admin.html', context)
     
     else:
@@ -156,7 +163,7 @@ def admin(request):
     
 
 def login_admin(request):
-    if 'username' in request.session:
+    if 'thanseeeeeh' in request.session:
         return redirect(admin)
     if request.method == 'POST':
         username = request.POST['username']
@@ -164,7 +171,7 @@ def login_admin(request):
 
         if username == 'thanseeeeeh' and password == 'ellikkal7':
             # valid credentials, log in the user and redirect to admin page
-            request.session['username'] = username
+            request.session['thanseeeeeh'] = username
             return redirect('admin')
         else:
             messages.info(request, 'Invalid Username or Password')
@@ -182,45 +189,70 @@ def delete_user(request, user_id):
 def update_user(request, user_id):
     user = User.objects.get(id=user_id)
     user.delete()
-
+    
     if request.method == 'POST':
         first_name = request.POST['first_name']
-        email = request.POST['email']
+        username = request.POST['username']
+        email =request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
 
-        # validate the input fields
-        if first_name.strip() == "" or email.strip() == "":
+        # space validation 
+        if first_name.strip() == "" or username.strip() == "" or email.strip() == "" or password.strip() == "" or confirm_password.strip() == "":
             messages.info(request, 'Enter the required Fields')
-            return redirect('update_user', user_id=user_id)
-
+            return redirect(update_user)
+        
+        # first_name validation
         if not re.match(r'^[a-zA-Z]{3,20}$', first_name):
             messages.info(request, 'First name must be between 3 and 20 characters and contain only alphabets')
-            return redirect('update_user', user_id=user_id)
-
+            return redirect(update_user)
+        
+        # Username validation
+        if not re.match(r'^.{3,20}$', username):
+            messages.info(request, 'Username must be between 3 and 20 characters')
+            return redirect(update_user)
+        
+        # Email validation
         if not re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', email):
             messages.info(request, 'Invalid email format')
-            return redirect('update_user', user_id=user_id)
-
-        # update the user
-        user.first_name = first_name
-        user.email = email
-        user.save()
-
-        messages.success(request, 'User updated successfully')
-        return redirect('admin')
-
-    context = {'user': user}
-    return render(request, 'update_user.html', context)
+            return redirect(update_user)
+        
+        # Password validation
+        if not re.match(r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,20}$', password):
+            messages.info(request, 'Password must contain at least one letter and one digit, and be between 5 and 20 characters')
+            return redirect(update_user)
+        
+        if password==confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username is already taken')
+                return redirect(update_user) 
+            
+            if  User.objects.filter(email=email).exists():
+                messages.info(request, 'Email is already taken')
+                return redirect(update_user) 
+            
+            else:
+                user = User.objects.create_user(first_name=first_name, username=username, email=email, password=password)
+                user.set_password(password) 
+                user.save()
+                return redirect('admin')
+        
+        else:
+            messages.info(request, 'Both passwords are not matching')
+            return redirect(update_user)
+    else:
+        context = {'user': user}
+        return render(request, 'update_user.html', context)
 
     
 
 
 def index(request):
     if 'username' in request.session:
-        return render(request, 'index.html')
+        music_data = Musics.objects.all()
+        context = {'music_data': music_data}
+        return render(request, 'index.html', context)
     else:
         return render(request, 'login.html')
-
-    
-    
 
 
